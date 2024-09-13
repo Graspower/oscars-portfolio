862a2ba39a4f055a1e6c16b96d7bda2b91176301
Your Name <you@example.com>
2024-09-14 01:56:09 +0300

    upd

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings: ['Sofware Engineerng', 'Front-end developer', 'Team Lead'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+    });
